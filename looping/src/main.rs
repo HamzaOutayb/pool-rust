7d582a9d22aa092efb2e15f563d5cc2ae2d4aca0
Hamza Outayb <houtaib@gmail.com>
2025-05-08 11:27:39 +0100

    looping

---
+++ looping/src/main.rs
@@ -0,0 +1,17 @@
+use std::io;
+fn main() {
+    let text = String::from("I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?");
+    println!("{}", text);
+    let mut trials = 0;
+    loop {
+        let mut input = String::new();
+       let _ = io::stdin().read_line(&mut input);
+        if input.trim() == "The letter e" {
+            println!("Number of trials: {}", trials);
+            break
+        } else {
+            trials += 1;
+            println!("{}", text);
+        }
+    }
+}
