5bf15cba06ebfab40faa1085460237dac2279568
Hamza Outayb <houtaib@gmail.com>
2025-05-08 12:45:14 +0100

    matrix

--- matrix_transposition/src/lib.rs
+++ matrix_transposition/src/lib.rs
@@ -1,6 +1,8 @@
 #[derive(PartialEq, Eq)]
 #[derive(Debug)]
-struct Matrix((i32, i32)(i32, i32))
+pub struct Matrix(
+    pub (i32, i32),
+    pub (i32, i32))
 
 pub fn transpose(m: Matrix) -> Matrix {
     let matrix: Matrix = Matrix((m.1.0, m.1.1), (m.0.0, m.0.1))
