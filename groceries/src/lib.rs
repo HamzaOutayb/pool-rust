846e8a8e6955ceb1470c628de55b9cd418cce7e4
Hamza Outayb <houtaib@gmail.com>
2025-05-08 11:51:41 +0100

    grocerie

---
+++ groceries/src/lib.rs
@@ -0,0 +1,23 @@
+pub fn insert(vec: &mut Vec<String>, val: String) {
+    vec.push(val);
+}
+
+pub fn at_index(slice: &[String], index: usize) -> &str {
+    if let Some(v) = slice.get(index) {
+        v
+    } else {
+        "err"
+    }
+}
+
+// fn main() {
+//     let mut groceries = vec![
+//         "yogurt".to_string(),
+//         "panettone".to_string(),
+//         "bread".to_string(),
+//         "cheese".to_string(),
+//     ];
+//     insert(&mut groceries, String::from("nuts"));
+//     println!("groceries = {:?}", &groceries);
+//     println!("groceries[1] = {:?}", at_index(&groceries, 1));
+// }
