111713a83b16b9b03f03c8c262065a3332228b46
Hamza Outayb <houtaib@gmail.com>
2025-05-09 18:38:49 +0100

    tic-tao

---
+++ tic_tac_toe/src/lib.rs
@@ -0,0 +1,68 @@
+pub fn tic_tac_toe(table: [[char; 3]; 3]) -> String {
+    if horizontal('X', table) || vertical('X', table) {
+        return "X".to_string()
+    }
+    if horizontal('O', table) || vertical('O', table) {
+        return "O".to_string()
+    }
+    "tie".to_string()
+}
+
+pub fn diagonals(player: char, table: [[char; 3]; 3]) -> bool {
+    let mut count_main = 0;
+    let mut count_anti = 0;
+
+    for i in 0..3 {
+        if table[i][i] == player {
+            count_main += 1;
+        }
+
+        if table[i][2-i] == player {
+            count_anti += 1;
+        }
+    }
+
+    count_main == 3 || count_anti == 3
+}
+
+pub fn horizontal(player: char, table: [[char; 3]; 3]) -> bool {
+    for row in table {
+    let mut countplayer: i32 = 0;
+    for &cell in row.iter() {
+        if cell == player {
+            countplayer += 1;
+        }
+        if countplayer == 3 {
+            return true
+        }
+    }
+}
+ false
+}
+
+pub fn vertical(player: char, table: [[char; 3]; 3]) -> bool {
+let mut countplayer: i32 = 0;
+let mut from: i32 = 0;
+let mut to: i32 = 1;
+for _ in table {
+    for slc2 in table {
+    for hor in from..to {
+        if player == slc2[hor as usize] {
+            countplayer += 1;
+        }
+    }
+    if countplayer == 3 {
+        return true
+    }
+}
+
+if to == table.len().try_into().unwrap()  {
+    break
+}
+countplayer = 0;
+
+from += 1;
+to += 1;
+}
+return false 
+}
