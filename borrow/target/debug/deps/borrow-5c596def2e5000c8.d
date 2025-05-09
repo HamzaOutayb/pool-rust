a53ecaf7bb6495db67c4106ae84066f96bd52da3
Hamza Outayb <houtaib@gmail.com>
2025-05-09 10:22:35 +0100

    borrow

--- borrow/target/debug/deps/borrow-5c596def2e5000c8.d
+++
@@ -1,5 +0,0 @@
-/home/houtaib/Desktop/piscine-rust/borrow/target/debug/deps/libborrow-5c596def2e5000c8.rmeta: src/main.rs
-
-/home/houtaib/Desktop/piscine-rust/borrow/target/debug/deps/borrow-5c596def2e5000c8.d: src/main.rs
-
-src/main.rs:
--- borrow/target/debug/deps/borrow-dbd6664aee11686c.d
+++
@@ -1,5 +0,0 @@
-/home/houtaib/Desktop/piscine-rust/borrow/target/debug/deps/libborrow-dbd6664aee11686c.rmeta: src/main.rs
-
-/home/houtaib/Desktop/piscine-rust/borrow/target/debug/deps/borrow-dbd6664aee11686c.d: src/main.rs
-
-src/main.rs:
