e55f6f5602efd2654b79a11cc44175b9f3977c97
Hamza Outayb <houtaib@gmail.com>
2025-05-10 15:52:28 +0100

    cp2

--- capitalizing/src/lib.rs
+++ capitalizing/src/lib.rs
@@ -1,21 +1,31 @@
 pub fn capitalize_first(input: &str) -> String {
+    if input.is_empty() {
+        return "".to_string();
+    }
     (input.chars().next().expect("fff").to_ascii_uppercase()).to_string()+&input[1..]
 }
 
 pub fn title_case(input: &str) -> String {
-    let words: Vec<_> = input.split_whitespace().collect();
-    let mut res = String::new();
-    for (i, word) in words.into_iter().enumerate() {
-        let capitalized = (word.chars().next().expect("fff").to_ascii_uppercase()).to_string()+&(word[1..]).to_string();
-        if i != 0 {
-            res.push(' ')
+    if input.is_empty() {
+        return "".to_string();
+    }
+    let mut res: String = String::new();
+    let mut lastc = ' ';
+    for c in input.chars() {
+        if lastc == ' ' || lastc == '\t' {
+            res.push(c.to_ascii_uppercase());
+        } else{
+            res.push(c);
         }
-        res.push_str(&capitalized)
+        lastc = c
     }
     res
 }
 
 pub fn change_case(input: &str) -> String {
+    if input.is_empty() {
+        return "".to_string();
+    }
     let mut res: String = String::new();
 
     for c in input.chars() {
--- capitalizing/src/main.rs
+++ capitalizing/src/main.rs
@@ -5,8 +5,36 @@ fn main() {
     println!("{}", title_case("jill is leaving A"));
     println!("{}", change_case("heLLo THere"));
 }
-/*$ cargo run
-Joe is missing
-Jill Is Leaving A
-HEllO thERE
-$*/
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_capitalize_first() {
+        assert_eq!(capitalize_first("hello"), "Hello");
+        assert_eq!(capitalize_first("this is working"), "This is working");
+    }
+
+    #[test]
+    fn test_title_case() {
+        assert_eq!(title_case("this is a title"), "This Is A Title");
+        assert_eq!(
+            title_case("hello my\t\tname is carl"),
+            "Hello My\t\tName Is Carl"
+        );
+    }
+
+    #[test]
+    fn test_change_case() {
+        assert_eq!(change_case("PROgraming"), "proGRAMING");
+        assert_eq!(change_case("heLLo THere"), "HEllO thERE");
+    }
+
+    #[test]
+    fn test_empty() {
+        assert_eq!(capitalize_first(""), "");
+        assert_eq!(title_case(""), "");
+        assert_eq!(change_case(""), "");
+    }
+}
