289f2086bb4556fe84bd888e6a916d545b68e2ec
Hamza Outayb <houtaib@gmail.com>
2025-05-10 14:34:28 +0100

    strings

---
+++ strings/target/debug/deps/strings-36b93da57a13ba6a.d
@@ -0,0 +1,7 @@
+/home/houtaib/Desktop/piscine-rust/strings/target/debug/deps/libstrings-36b93da57a13ba6a.rmeta: src/lib.rs
+
+/home/houtaib/Desktop/piscine-rust/strings/target/debug/deps/libstrings-36b93da57a13ba6a.rlib: src/lib.rs
+
+/home/houtaib/Desktop/piscine-rust/strings/target/debug/deps/strings-36b93da57a13ba6a.d: src/lib.rs
+
+src/lib.rs:
---
+++ strings/target/debug/libstrings.d
@@ -0,0 +1,1 @@
+/home/houtaib/Desktop/piscine-rust/strings/target/debug/libstrings.rlib: /home/houtaib/Desktop/piscine-rust/strings/src/lib.rs
