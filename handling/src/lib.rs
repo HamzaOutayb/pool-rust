1fdcb2287724502e3520cfedeff5ecc010a64b46
houtaib <houtayb@gmail.com.com>
2025-05-15 16:27:26 +0100

    handling

--- handling/src/lib.rs
+++ handling/src/lib.rs
@@ -4,5 +4,5 @@ use std::fs::OpenOptions;
 
 pub fn open_or_create<P: AsRef<Path>>(path: &P, content: &str) {
     let file = OpenOptions::new().write(true).append(true).create(true).open(path);
-    file.unwrap().Write(content.as_bytes()).expect("hh");
+    file.unwrap().write(content.as_bytes()).expect("hh");
 }
