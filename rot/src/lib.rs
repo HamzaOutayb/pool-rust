725f5746c91da6d2a5dac02187ec39bbbbe03ad5
houtaib <houtayb@gmail.com.com>
2025-05-18 14:38:44 +0100

    rot

--- rot/src/lib.rs
+++ rot/src/lib.rs
@@ -4,11 +4,7 @@ pub fn rotate(input: &str, key: i8) -> String {
         if c.is_ascii_alphabetic() {
             if c.is_ascii_lowercase() {
                 let ch = c as u8 - b'a';
-                if key < 0 {
-                    res.push((((ch as i8+ key)%26 )as u8 + b'a') as char );
-                } else {
-                    res.push(((((ch as i8+ key)%26 )%26) as u8 + b'a') as char );
-                }
+                res.push(((((ch as i8+ key)%26+26 )%26) as u8 + b'a') as char );
             } else if c.is_ascii_uppercase() {
                 let ch = c as u8 - b'A';
                 res.push(((((ch as i8+ key)%26+26 )%26) as u8 + b'A') as char );
