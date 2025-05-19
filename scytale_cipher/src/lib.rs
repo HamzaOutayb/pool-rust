5300cb2c5e0c3d9f1a1b80a077501933aa74cd1c
houtaib <houtayb@gmail.com>
2025-05-19 18:23:33 +0100

    cipher

---
+++ scytale_cipher/src/lib.rs
@@ -0,0 +1,25 @@
+pub fn scytale_cipher(message: String, n: u32) -> String {
+    let mut res = String::new();
+    let mut rows: Vec<Vec<char>> = Vec::new();
+
+    for (i, c) in message.chars().enumerate() {
+        if i % n as usize == 0 {
+            rows.push(Vec::new());
+        }
+        rows.last_mut().unwrap().push(c);
+    }
+
+    let cols = n as usize;
+    for col in 0..cols {
+        for row in &rows {
+            if let Some(&ch) = row.get(col) {
+                res.push(ch);
+            } else {
+                res.push(' ');
+            }
+
+        }
+    }
+
+    res.trim().to_string()
+}
