d259ae89aa48635bcf8efac9b766c5301c52284b
houtaib <houtayb@gmail.com>
2025-06-12 10:37:08 +0100

    rev

--- reverse_string/src/lib.rs
+++ reverse_string/src/lib.rs
@@ -1,7 +1,3 @@
 pub fn rev_str(input: &str) -> String {
-    let mut res = String::new();
-    for c in input.chars().rev() {
-        res.push(c);
-    }
-    res
+    input.chars().rev().collect()
 }
