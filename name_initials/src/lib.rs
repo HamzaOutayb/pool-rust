e4c0cace4b56e584c506f89a6fdc8e813ebc4ab3
houtaib <houtayb@gmail.com>
2025-06-13 16:19:58 +0100

    NI

--- name_initials/src/lib.rs
+++ name_initials/src/lib.rs
@@ -2,8 +2,12 @@ pub fn initials(names: Vec<&str>) -> Vec<String> {
     let mut res = Vec::<String>::new();
     for name in names{
         let mut sub = Vec::<String>::new();
-        for e in name.split(" ") {
-                sub.push(e.chars().next().expect("dd").to_string() + ".")
+        let mut last = ' ';
+        for char in name.chars() {
+            if last == ' ' {
+                sub.push(format!("{}.", char.to_ascii_uppercase()));
+                last = char;
+            }
         }
         res.push(sub.join(" "))
     }
