f98fb518bf3d3ab80326899275dcf5d7b02162dc
houtaib <houtayb@gmail.com>
2025-06-14 11:57:05 +0100

    hashmap

--- simple_hash/src/lib.rs
+++ simple_hash/src/lib.rs
@@ -1,12 +1,12 @@
 use std::collections::HashMap;
 
 pub fn word_frequency_counter<'a>(words: &[&'a str]) -> HashMap<&'a str, usize> {
+    // let mut res: HashMap<&'a str, usize> = HashMap::new();
     let mut res: HashMap<&'a str, usize> = HashMap::new();
 
     for word in words {
         *res.entry(word).or_insert(0) += 1
     }
-
     res
 }
 
