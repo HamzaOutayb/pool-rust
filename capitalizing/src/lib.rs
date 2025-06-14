68b9a6d8ca32387649c2e56c2c205d10b7e3def4
houtaib <houtayb@gmail.com>
2025-06-14 10:28:16 +0100

    cap

--- capitalizing/src/lib.rs
+++ capitalizing/src/lib.rs
@@ -2,40 +2,33 @@ pub fn capitalize_first(input: &str) -> String {
     if input.is_empty() {
         return "".to_string();
     }
-    (input.chars().next().expect("fff").to_ascii_uppercase()).to_string()+&input[1..]
+    (input.chars().next().unwrap().to_string() + &input[1..]).to_string()
 }
 
 pub fn title_case(input: &str) -> String {
-    if input.is_empty() {
-        return "".to_string();
-    }
     let mut res: String = String::new();
-    let mut lastc = ' ';
-    for c in input.chars() {
-        if lastc == ' ' || lastc == '\t' {
-            res.push(c.to_ascii_uppercase());
-        } else{
-            res.push(c);
+    let mut last = ' ';
+    for char in input.chars() {
+        if last == ' ' {
+            res.push(char.to_ascii_uppercase());
+        } else {
+            res.push(char);
         }
-        lastc = c
+        last = char
     }
     res
 }
 
 pub fn change_case(input: &str) -> String {
-    if input.is_empty() {
-        return "".to_string();
-    }
     let mut res: String = String::new();
-
-    for c in input.chars() {
-            if c.is_uppercase() {
-                res.push(c.to_ascii_lowercase());
-            } else if c.is_lowercase() {
-                res.push(c.to_ascii_uppercase());
-            } else {
-                res.push(c);
-            }
+    for char in input.chars() {
+        if char.is_ascii_uppercase() {
+            res.push(char.to_ascii_lowercase())
+        } else if char.is_ascii_lowercase() {
+            res.push(char.to_ascii_uppercase())
+        } else {
+            res.push(char)
+        }
     }
     res
 }
