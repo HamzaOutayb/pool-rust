b9720d4377e9a73a7660446e3423b57d8e82f809
houtaib <houtayb@gmail.com>
2025-06-19 14:33:12 +0100

    error_types

--- error_types/src/lib.rs
+++ error_types/src/lib.rs
@@ -90,36 +90,38 @@ pub struct Form {
 }
 
 impl Form {
-    pub fn new(name: String, password: String) -> self {
-        Self {
-            name,
-            password,
-        }
-    }
-    pub fn validate(&self) -> Result<(), FormError> {
+       pub fn validate(&self) -> Result<(), FormError> {
         let mut alphabitic = false;
         let mut symbols = false;
         let mut numeric = false;
 
         for c in self.password.chars() {
-            if c.is_ascii_uppercase() || c.is_ascii_lowercase() {
+            if c.is_ascii_lowercase() || c.is_ascii_uppercase() {
                 alphabitic = true;
             } else if c.is_ascii_punctuation() {
                 symbols = true;
-            } else if c.is_ascii_digit() {
+            } else if c.is_ascii_digit(){
                 numeric = true;
-            };
-            if self.name.is_empty() {
-                return Err(FormError::new("name", "Username is empty"));
             }
+        };
+        
+        if self.name.is_empty() {
+            return Err(FormError::new("name",self.name.clone(),"Username is empty"));
+        };
 
-            if alphabitic && symbols && numeric && self.password >= 8 {
-                return Ok(());
-            }
+        if alphabitic && symbols && numeric && self.password.len() >= 8 {
+            return Ok(());
+        }; 
 
-            if self.password < 8 {
-                return Err()
-            }
-        }
+
+        if self.password.len() < 8 {
+            return Err(FormError::new("password", self.password.clone(), "Password should be at least 8 characters long"));
+        };
+
+        if !alphabitic || !symbols || !numeric {
+            return Err(FormError::new("password", self.password.clone(), "Password should be a combination of ASCII numbers, letters and symbols"));
+        };
+
+        Ok(())
     }
 }
