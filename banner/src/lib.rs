44c68072c0424a326fa664da23594fc111f99a76
houtaib <houtayb@gmail.com>
2025-06-19 12:21:14 +0100

    banner

--- banner/src/lib.rs
+++ banner/src/lib.rs
@@ -1,35 +1,18 @@
-use std::num::ParseFloatError;
-use std::collections::HashMap;
-
-pub fn div(a: &str, b: &str) -> Result<String, ParseFloatError> {
-    let c = a.parse::<f64>()?;
-    let d = b.parse::<f64>()?;
-    let div: f64 = c as f64 /d as f64;
-    Ok((div).to_string())
-}
-
-pub fn rem(a: &str, b: &str) -> Result<String, ParseFloatError> {
-    let c = a.parse::<f64>()?;
-    let d = b.parse::<f64>()?;
-    let rim: f64 = c as f64 %d as f64;
-    Ok((rim).to_string())
-}
-
 use std::{collections::HashMap, num::ParseFloatError};
 
 pub struct Flag {
-    pub short_hand: String
+    pub short_hand: String,
 
-    pub long_hand: String
+    pub long_hand: String,
 
-    pub desc: String
+    pub desc: String,
 }
 
-impl<'a> Flag<'a> {
+impl<'a> Flag {
     pub fn opt_flag(name: &'a str, d: &'a str) -> Self {
         Self {
-            short_hand: format!("-", &name[0..1]),
-            long_hand: format!("--", name),
+            short_hand: format!("-{}", &name[0..1]),
+            long_hand: format!("--{}", name),
             desc: d.to_string(),
         }
     }
@@ -43,8 +26,8 @@ pub struct FlagsHandler {
 
 impl FlagsHandler {
     pub fn add_flag(&mut self, flag: Flag, func: Callback) {
-        self.falgs.insert(flag.short_hand.clone(), func);
-        self.falgs.insert(flag.long_hand.clone(), func);
+        self.flags.insert(flag.short_hand.clone(), func);
+        self.flags.insert(flag.long_hand.clone(), func);
     }
 
     pub fn exec_func(&self, input: &str, argv: &[&str]) -> Result<String, String> {
