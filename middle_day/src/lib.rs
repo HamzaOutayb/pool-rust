fc8228490ca984ea8bfba0ee78447f74c0ead2d4
houtaib <houtayb@gmail.com>
2025-06-21 10:07:12 +0100

    mD

--- middle_day/src/lib.rs
+++ middle_day/src/lib.rs
@@ -1,3 +1,15 @@
+// use chrono::NaiveDate;
+// use chrono::Datelike;
+
+// pub fn middle_day(year: u32) -> Option<chrono::Weekday> {
+//     let date = NaiveDate::from_ymd_opt(year as i32, 7, 2);
+//     if !date?.leap_year() {
+//         Some(date?.weekday())
+//     } else {
+//         None
+//     }
+// }
+
 use chrono::NaiveDate;
 use chrono::Datelike;
 
