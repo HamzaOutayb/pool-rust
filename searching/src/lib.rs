d20c9130ded6ea86f8f13713933eea8eb4bf5ea4
houtaib <houtayb@gmail.com>
2025-06-21 14:34:03 +0100

    searching

--- searching/src/lib.rs
+++ searching/src/lib.rs
@@ -1,9 +1,17 @@
 pub fn search(array: &[i32], key: i32) -> Option<usize> {
-    for (i, el) in array.iter().enumerate() {
-        match *el == key {
+    // for (i, el) in array.iter().enumerate() {
+    //     match *el == key {
+    //         true => return Some(i),
+    //         false => continue,
+    //     }
+    // }
+    // None
+
+    for (i, e) in array.iter().enumerate() {
+        match *e == key {
             true => return Some(i),
             false => continue,
-        }
+        } 
     }
     None
 }
