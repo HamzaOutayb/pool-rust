a94e71d6c26aceaec9c0973fe248f4c437032b72
houtaib <houtayb@gmail.com>
2025-06-21 14:38:05 +0100

    or

--- ordinal/src/lib.rs
+++ ordinal/src/lib.rs
@@ -1,13 +1,28 @@
-pub fn num_to_ordinal(x: u32) -> String {
-    if x % 100 == 11 || x % 100 == 12 || x % 100 == 13 {
-        return format!("{}th", x);
-    };
+use std::fmt::format;
 
-    let ordinal = match x % 10 {
+pub fn num_to_ordinal(x: u32) -> String {
+    // if x % 100 == 11 || x % 100 == 12 || x % 100 == 13 {
+    //     return format!("{}th", x);
+    // };
+
+    // let ordinal = match x % 10 {
+    //     1 => "st",
+    //     2 => "nd",
+    //     3 => "rd",
+    //     _ => "th",
+    // };
+
+    // format!("{}{}", x, ordinal)
+
+    if x%100 == 11 || x%100 == 12 || x%100 == 13 {
+        return format!("{}th", x);
+    }
+
+    let ordinal = match x%10 {
         1 => "st",
         2 => "nd",
         3 => "rd",
-        _ => "th",
+        _=> "th"
     };
 
     format!("{}{}", x, ordinal)
