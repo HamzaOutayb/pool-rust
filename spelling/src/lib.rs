2d608843a8e6288b97ab11cd86144f6a0ae2bd92
houtaib <houtayb@gmail.com>
2025-06-22 16:04:22 +0100

    spelling

--- spelling/src/lib.rs
+++ spelling/src/lib.rs
@@ -2,52 +2,58 @@ pub fn spell(n: u64) -> String {
     if n == 0 {
         return "zero".to_string();
     }
-    if n == 1000000 {
-        return "one million".to_string()
+    if n == 1_000_000 {
+        return "one million".to_string();
     }
 
     let units = [
         "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
     ];
     let teens = [
-        "ten",
-        "eleven",
-        "twelve",
-        "thirteen",
-        "fourteen",
-        "fifteen",
-        "sixteen",
-        "seventeen",
-        "eighteen",
-        "nineteen",
+        "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen",
+        "sixteen", "seventeen", "eighteen", "nineteen",
     ];
     let tens = [
-        "", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety",
+        "", "", "twenty", "thirty", "forty", "fifty",
+        "sixty", "seventy", "eighty", "ninety",
     ];
 
+    let spell_under_1000 = |n: u64| -> Vec<String> {
+        let mut res = Vec::new();
 
-    let func = |n| {
-        let mut res = vec![];
-        if n < 10 {
-            res.push(units[n as usize].to_string());
-        } else if n < 20 {
-            res.push(teens[n as usize - 10].to_string());
-        } else if n < 100 {
-            res.push(tens[n as usize / 10].to_string() + "-" + units[n as usize % 10]);
-        } else {
-            res.push(units[n as usize / 100].to_string());
+        if n >= 100 {
+            res.push(units[(n / 100) as usize].to_string());
             res.push("hundred".to_string());
-            res.push(tens[n as usize / 100].to_string() + "-" + units[n as usize % 10]);
         }
+
+        let n = n % 100;
+        if n >= 20 {
+            let mut t = tens[(n / 10) as usize].to_string();
+            if n % 10 != 0 {
+                t.push('-');
+                t.push_str(units[(n % 10) as usize]);
+            }
+            res.push(t);
+        } else if n >= 10 {
+            res.push(teens[(n - 10) as usize].to_string());
+        } else if n > 0 {
+            res.push(units[n as usize].to_string());
+        }
+
         res
     };
 
-    
-    let mut res = func(n/1000);
-    if n >= 1000{
-        res.push("thousand".to_string());
-    }
-    res.extend(func(n%1000));
+    let mut result = Vec::new();
 
-    res.join(" ").trim().to_string()
+    if n >= 1000 {
+        result.extend(spell_under_1000(n / 1000));
+        result.push("thousand".to_string());
+    }
+
+    let rem = n % 1000;
+    if rem != 0 {
+        result.extend(spell_under_1000(rem));
+    }
+
+    result.join(" ")
 }
