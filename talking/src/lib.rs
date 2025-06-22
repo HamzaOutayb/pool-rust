f7df33b46c70998769d6939bfabbc3dcd7c7257b
houtaib <houtayb@gmail.com>
2025-06-22 10:28:58 +0100

    talking

--- talking/src/lib.rs
+++ talking/src/lib.rs
@@ -1,23 +1,48 @@
+// pub fn talking(text: &str) -> &str {
+//     if text.trim() == "" {
+//         return "Just say something!"
+//     }
+//     let mut is_uppercase = false;
+//     for c in text.chars() {
+//         if c.is_ascii_alphabetic() && c.is_ascii_uppercase(){
+//                 is_uppercase = true;
+//         } else if c.is_ascii_alphabetic() && c.is_ascii_lowercase() {
+//             is_uppercase = false;
+//             break
+//         }
+//     }
+
+//      if is_uppercase && &text[text.len()-1..text.len()] == "?" {
+//         return "Quiet, I am thinking!"
+//     }else if !is_uppercase && &text[text.len()-1..] == "?"{
+//         return "Sure."
+//     }  else if is_uppercase{
+//         return "There is no need to yell, calm down!"
+//     } else {
+//         return "Interesting"
+//     }
+// }
+
 pub fn talking(text: &str) -> &str {
-    if text.trim() == "" {
+    if text.is_empty() {
         return "Just say something!"
     }
-    let mut is_uppercase = false;
+    let mut upper = false;
     for c in text.chars() {
-        if c.is_ascii_alphabetic() && c.is_ascii_uppercase(){
-                is_uppercase = true;
-        } else if c.is_ascii_alphabetic() && c.is_ascii_lowercase() {
-            is_uppercase = false;
+        if c.is_ascii_alphabetic() && c.is_ascii_lowercase() {
+            upper = false;
             break
+        } else if c.is_ascii_alphabetic() && c.is_ascii_uppercase() {
+            upper = true
         }
     }
 
-     if is_uppercase && &text[text.len()-1..text.len()] == "?" {
+    if upper && &text[text.len()-1..] ==  "?" {
         return "Quiet, I am thinking!"
-    }else if !is_uppercase && &text[text.len()-1..] == "?"{
+    } else if !upper && &text[text.len()-1..] ==  "?" {
         return "Sure."
-    }  else if is_uppercase{
-        return "There is no need to yell, calm down!"
+    } else if upper {
+        return  "There is no need to yell, calm down!"
     } else {
         return "Interesting"
     }
