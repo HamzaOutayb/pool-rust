855d13b3d561c841f65291ba2753f61e44c8995e
houtaib <houtayb@gmail.com>
2025-06-22 13:19:42 +0100

    scytale_cipher

--- scytale_cipher/src/lib.rs
+++ scytale_cipher/src/lib.rs
@@ -1,14 +1,15 @@
 pub fn scytale_cipher(message: String, n: u32) -> String {
-    let mut res = String::new();
     let mut rows: Vec<Vec<char>> = Vec::new();
-
+    
     for (i, c) in message.chars().enumerate() {
         if i % n as usize == 0 {
             rows.push(Vec::new());
         }
         rows.last_mut().unwrap().push(c);
+        println!("{:?}", rows);
     }
-
+    
+    let mut res = String::new();
     let cols = n as usize;
     for col in 0..cols {
         for row in &rows {
