a8738fd96a30bdc9828f90f8827a0b9a0184866d
houtaib <houtayb@gmail.com>
2025-06-22 11:38:33 +0100

    pig_latine

--- pig_latin/src/lib.rs
+++ pig_latin/src/lib.rs
@@ -1,13 +1,14 @@
 pub fn pig_latin(text: &str) -> String {
-    let vawols = "aeiou";
-    if !vawols.contains(text.chars().next().unwrap()) && text[1..3] == "qu".to_string() {
-        return text[3..].to_string() + &text[..3].to_string()+ "ay";
+    let vowels = String::from("aeiou");
+
+    if !vowels.contains(text.chars().next().unwrap()) && &text[1..3] == "qu" {
+        return text[3..].to_string() + &text[..3].to_string() + "ay"
     }
 
     for (i, c) in text.chars().enumerate() {
-        if "aeiou".contains(c) {
-            return text[i..].to_string() + &text[..i].to_string() + "ay";
+        if vowels.contains(c) {
+            return text[i..].to_string() + &text[i..].to_string() + "ay"
         }
     }
     text.to_string()
-}
+} 
